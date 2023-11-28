7bf73bc1637228e6f4776e35dabad9fb1d51e8ec
Miguel Marcano <miguel131299@gmail.com>
2023-11-28 17:14:29 +0100

    Add basic frontend app structure

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,16 @@
+import "../styles/LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="lp-container ">
+      <div className="title-container content-page">
+        <h1 className="main-title">
+          Caturanga: Empowering Progress, Enriching Lives: Together for a Better
+          World
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
