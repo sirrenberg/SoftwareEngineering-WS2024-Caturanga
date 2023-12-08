f58efb4b44a4e354dc738b748b3ababec662f890
nicleosch <147690011+nicleosch@users.noreply.github.com>
2023-12-08 12:51:58 +0100

    Feature log all locations (#39)
    * update flee
    
    * use burundi csvs for simulation
    
    * store simulation results in database

--- backend/flee_adapter/adapter.py
+++ backend/flee_adapter/adapter.py
@@ -1,7 +1,9 @@
 import subprocess
 import os
 import pandas as pd
-from runscripts.run import Simulation
+from runscripts.runner import Simulation
+from dotenv import load_dotenv
+from pymongo import MongoClient
 
 class Adapter:
     def __init__(self):
@@ -9,8 +11,23 @@ class Adapter:
         pass
     
     def run_simulation(self):
-        sim = Simulation("flee/test_data/test_input_csv", "flee/test_data/test_input_csv/refugee_data", 0, "flee/test_data/simsetting.yml")
+        sim = Simulation("flee/conflict_input/burundi", "flee/conflict_validation/burundi2015", 0, "flee/test_data/simsetting.yml")
         result = sim.run()
+        self.store_simulation(result)
         
         return result
     
+    # TODO: This method should be moved to the flee controller
+    def store_simulation(self, result):
+
+        load_dotenv()
+        MONGODB_URI = os.environ.get('MONGO_URI')
+        client = MongoClient(MONGODB_URI)
+        db = client.Caturanga
+        simulations_collection = db.simulations_results
+
+        new_simulation = {}
+        new_simulation["data"] = result
+        simulations_collection.insert_one(new_simulation)
+
+        client.close()
