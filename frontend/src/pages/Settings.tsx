a7da7696624c016cd89587f92b1d7253d18ba3ef
Klara Cimbalnik <k.cimbalnik@t-online.de>
2023-12-14 00:31:40 +0100

    Improve Settings page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,22 +1,31 @@
+import { InputAdornment, Stack } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
 function Settings() {
   return (
     <div className="content-page">
-      <TextField
-        id="max-move-speed"
-        label="Max Move Speed"
-        type="number"
-        defaultValue="360"
-        helperText="the maximum number of kilometers expected to be travelled by IDPs per time step (30 km /h for 12 hours)"
-      />
-      <TextField
-        id="max-walk-speed"
-        label="Max Walk Speed"
-        type="number"
-        defaultValue="35"
-        helperText="the maximum number of kilometers expected to be travelled per time step on foot (3.5 km/h for 10 hours)"
-      />
+      <Stack spacing={3}>
+        <TextField
+          id="max-move-speed"
+          label="Max Move Speed"
+          type="number"
+          defaultValue="360"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">km</InputAdornment>
+          }}
+          helperText="the maximum number of kilometers expected to be travelled by IDPs per time step (30 km /h for 12 hours)"
+        />
+        <TextField
+          id="max-walk-speed"
+          label="Max Walk Speed"
+          type="number"
+          defaultValue="35"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">km</InputAdornment>
+          }}
+          helperText="the maximum number of kilometers expected to be travelled per time step on foot (3.5 km/h for 10 hours)"
+        />
+      </Stack>
     </div>
   );
 }
