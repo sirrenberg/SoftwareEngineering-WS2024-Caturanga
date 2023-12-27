b839ca0d1962ebc3762f1f5993c74a9df1aafb68
Jonas <jonas.maurer@campus.lmu.de>
2023-12-27 19:49:29 +0100

    create empty closure file

---
+++ backend/input_data_processing/create_closures_csv.py
@@ -0,0 +1,19 @@
+import os 
+import csv
+
+def create_empty_closure_csv(folder_name):
+    # Get the current directory
+    current_dir = os.getcwd()
+    #open locations.csv
+    closures_file = os.path.join(current_dir, folder_name, "closures.csv")
+
+    # write in closures_file.csv
+    with open(closures_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['#closure_type', 'name1', 'name2', 'closure_start', 'closure_end'])
+    
+    print(f'{folder_name}/closures.csv created.')
+
+
+    
+
