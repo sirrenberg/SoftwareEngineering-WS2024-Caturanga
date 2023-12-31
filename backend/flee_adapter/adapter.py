526bbb50a31bd7358ce12f9abe7e7d2e80d3ee40
Nicolas Schmitt <ge93cuy@mytum.de>
2023-12-31 16:00:58 +0100

    Add init.py to establish package structure

--- backend/flee_adapter/adapter.py
+++ backend/flee_adapter/adapter.py
@@ -1,11 +1,4 @@
-import pathlib
-import sys
-
-# add flee directory to PYTHONPATH
-backend = pathlib.Path(__file__).parent.parent
-sys.path.insert(0, str(backend / "flee"))
-
-from flee.runscripts.runner import Simulation  # noqa: E402
+from runscripts.runner import Simulation  # noqa: E402
 
 class Adapter:
     def __init__(self):
