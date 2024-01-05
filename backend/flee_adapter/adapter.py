521ad12b3008248ffa3a2be69fb82b458795f2ce
Nicolas Schmitt <ge93cuy@mytum.de>
2024-01-05 22:26:07 +0100

    add documentation

--- backend/flee_adapter/adapter.py
+++ backend/flee_adapter/adapter.py
@@ -1,26 +1,66 @@
 from runscripts.runner import Simulation
 
+
 class Adapter:
+    """
+    Adapter class for running simulations using the FLEE library.
+    """
+
     def __init__(self):
         # This method is intentionally left empty
         pass
 
-    # Run Simulations: -------------------------------------------------------------------------------------------------
-
-    # Run default simulation (burundi, test-simsettings):
     def run_simulation(self):
-        sim = Simulation("flee/conflict_input/burundi", "flee/conflict_validation/burundi2015", 0, "flee/test_data/simsetting.yml")
+        """
+        Runs a default simulation using the FLEE library.
+
+        Returns:
+        - dict: The result of the simulation.
+        """
+        sim = Simulation("flee/conflict_input/burundi",
+                         "flee/conflict_validation/burundi2015",
+                         0,
+                         "flee/test_data/simsetting.yml")
         result = sim.run()
+
         return result
 
-    # Run Simulation with custom simsettings:
-    def run_simulation_ss(self, simsettings_file: str):
-        sim = Simulation("flee/conflict_input/burundi", "flee/conflict_validation/burundi2015", 0, simsettings_file)
+    def run_simulation_ss(self,
+                          simsettings_file: str):
+        """
+        Runs a simulation using custom simsettings.
+
+        Parameters:
+        - simsettings_file (str): The path to the simsettings file.
+
+        Returns:
+        - dict: The result of the simulation.
+        """
+        sim = Simulation("flee/conflict_input/burundi",
+                         "flee/conflict_validation/burundi2015",
+                         0,
+                         simsettings_file)
         result = sim.run()
+
         return result
 
-    # Run Simulation with custom simulation and simsettings:
-    def run_simulation_config(self, simulation_dir: str, simsettings_file: str):
-        sim = Simulation(simulation_dir, "flee/conflict_validation/burundi2015", 0, simsettings_file)
+    def run_simulation_config(self,
+                              simulation_dir: str,
+                              simsettings_file: str):
+        """
+        Runs a simulation using custom simulation and simsettings.
+
+        Parameters:
+        - simulation_dir (str): The path to the simulation directory.
+        - simsettings_file (str): The path to the simsettings file.
+
+        Returns:
+        -dict: The result of the simulation.
+        """
+        sim = Simulation(simulation_dir,
+                         "flee/conflict_validation/burundi2015",
+                         0,
+                         simsettings_file)
         result = sim.run()
+
         return result
