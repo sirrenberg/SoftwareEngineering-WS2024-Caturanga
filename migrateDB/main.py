848a34f88dca4fc461acbd1a2de5861dfd01a541
Nils Sirrenberg <nils.sirrenberg@web.de>
2024-01-08 18:19:46 +0100

    added tool to migrate data from atlas to documentdb

--- migrateDB/main.py
+++ migrateDB/main.py
@@ -28,11 +28,11 @@ def read_root():
 @app.get("/username")
 def read_user():
     logging.debug("Trying to invoke get_secret() from main.py...")
-    username = get_secret()
+    username = get_secret("caturanga-db-user-and-pw")
     logging.debug(
         "get_secret() invoked from main(). Username is of type: " + str(type(username))
     )
-    return {"username": get_secret()["username"]}
+    return {"username": get_secret("caturanga-db-user-and-pw")["username"]}
 
 @app.get("/simsettings")
 def get_all_simsettings():
@@ -42,6 +42,17 @@ def get_all_simsettings():
     for simsetting in simsettings:
         simsetting["_id"] = str(simsetting["_id"])
         rl.append(simsetting)
+    client.close()
+    return rl
+
+@app.get("/simulations")
+def get_all_simulations():
+    client, db = connect_db()
+    simulations = db.get_collection("simulations").find({})
+    rl = []
+    for simulation in simulations:
+        simulation["_id"] = str(simulation["_id"])
+        rl.append(simulation)
     client.close()
     return rl
 
@@ -64,12 +75,11 @@ async def get_all_simulation_results():
     client.close()
     return rl
 
-def get_secret():
+def get_secret(secret_name):
     """
     Retrieves the secret value that contains the username and password for the documentDB database from the AWS Secrets Manager.
     """
     logging.basicConfig(level=logging.DEBUG)
-    secret_name = "caturanga-db-user-and-pw"
     region_name = "eu-west-1"
 
     # Create a Secrets Manager client
@@ -116,7 +126,7 @@ def connect_db():
         tuple: A tuple containing the MongoClient object and the database object.
     """
     load_dotenv()
-    secret = get_secret()
+    secret = get_secret("caturanga-db-user-and-pw")
     username = secret["username"]
     password = secret["password"]
     MONGODB_URI = f"mongodb://{username}:{password}@caturanga-2023-12-08-16-18-00.cluster-cqhcnfxitkih.eu-west-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
@@ -132,7 +142,10 @@ def connect_atlas_db():
         tuple: A tuple containing the MongoClient object and the database object.
     """
     load_dotenv()
-    MONGODB_URI = "mongodb+srv://sirrenberg:9x4iPEJ3jDvk11Fv@caturanga.o94xzij.mongodb.net/?retryWrites=true&w=majority"
+    secret = get_secret("password-for-atlas")
+    username = secret["username"]
+    password = secret["password"]
+    MONGODB_URI = f"mongodb+srv://{username}:{password}@caturanga.o94xzij.mongodb.net/?retryWrites=true&w=majority"
     atlas_client = MongoClient(MONGODB_URI)
     atlas_db = atlas_client.Caturanga
     return atlas_client, atlas_db
@@ -155,18 +168,58 @@ def migrate_from_atlas():
     atlas_client.close()
     return "Migration successful."
 
-@app.get("/docs")
+@app.get("/delete_all_in_document_db")
+def delete_all_in_document_db():
+    """
+    Insert all data from the Atlas database into the Amazon DocumentDB database.
+    """
+    aws_ddb_client, aws_ddb_db = connect_db()
+    collection_names = aws_ddb_db.list_collection_names()
+    for collection_name in collection_names:
+        aws_ddb_db[collection_name].drop()
+    aws_ddb_client.close()
+    return "Deletion successful."
+
+@app.get("/documents")
 def atlas_documents():
     """
     Insert all data from the Atlas database into the Amazon DocumentDB database.
     """
     atlas_client, atlas_db = connect_atlas_db()
     atlas_collection_names = atlas_db.list_collection_names()
+    docs = []
     for collection_name in atlas_collection_names:
         atlas_collection = atlas_db.get_collection(collection_name)
         atlas_documents = atlas_collection.find({})
         for document in atlas_documents:
-            print(document)
+            docs.append(document)
 
     atlas_client.close()
-    return "Migration successful."
+    return docs
+
+@app.get("/dsimsettings/{simsetting_id}")
+async def delete_simsetting(simsetting_id: str):
+    client, db = connect_db()
+    db.get_collection("simsettings").delete_one(
+        {"_id": ObjectID(simsetting_id)}
+    )
+    client.close()
+    return 
+
+@app.get("/dsimulationresults/{results_id}")
+async def delete_simulationresults(results_id: str):
+    client, db = connect_db()
+    db.get_collection("simulations_results").delete_one(
+        {"_id": ObjectID(results_id)}
+    )
+    client.close()
+    return 
+
+@app.get("/dsimulations/{sim_id}")
+async def delete_simulations(sim_id: str):
+    client, db = connect_db()
+    db.get_collection("simulations").delete_one(
+        {"_id": ObjectID(sim_id)}
+    )
+    client.close()
+    return 
