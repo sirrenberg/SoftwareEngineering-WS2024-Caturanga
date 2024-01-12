8ffafef2e515ecc2603cf6477feed9645a190628
Nils Sirrenberg <nils.sirrenberg@web.de>
2024-01-12 19:20:11 +0100

    changing vite settings from development to production

--- frontend/src/hooks/useAPI.tsx
+++ frontend/src/hooks/useAPI.tsx
@@ -1,5 +1,5 @@
 export function useAPI() {
-  const API_URL = "http://34.254.183.97";
+  const API_URL = "https://984ft56931.execute-api.eu-west-1.amazonaws.com/backend";
 
   function sendRequest(route: string, method: string, body?: any) {
     return fetch(API_URL + route, {
