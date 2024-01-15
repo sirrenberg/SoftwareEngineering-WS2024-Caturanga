6f5c437b5bb2aab3c0f4e03e2f891cdc70d4a291
Klara Cimbalnik <k.cimbalnik@t-online.de>
2024-01-15 15:20:30 +0100

    Unnest h2 out of h1 on Landing Page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -13,8 +13,8 @@ function LandingPage() {
           <div className="lp-title-container lp-title-content-section">
             <h1 className="main-title">
               Caturanga
-              <h2 className="sub-title">A Humanitarian Logistics Tool</h2>
             </h1>
+            <h2 className="sub-title">A Humanitarian Logistics Tool</h2>
 
             <p className="lp-title-description">
               Experience the power of Compassion in Crisis through Interactive
