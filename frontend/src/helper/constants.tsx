7a4834244362a6a42fceb76b6d184196952ce767
Klara Cimbalnik <k.cimbalnik@t-online.de>
2024-01-16 18:30:27 +0100

    Adjust color scheme of website
    Also adjusted how the status is displayed on the results page.

--- frontend/src/helper/constants.tsx
+++ frontend/src/helper/constants.tsx
@@ -198,3 +198,16 @@ export const spawnRulesText =
     The following section contains the parameters that influence their spawning during the simulation.
     `
 ;
+
+export enum Colors {
+    light_green = "#D8FFD6",
+    medium_green = "#71A480",
+    dark_green = "#1D493D",
+    light_blue = "#AFCBD4",
+    medium_blue = "#476C85",
+    dark_blue = "#2C4251",
+    light_orange = "#F3B391",
+    medium_orange = "#EA7734",
+    dark_orange = "#95400F",
+    white = "#FFFFFF",
+}
