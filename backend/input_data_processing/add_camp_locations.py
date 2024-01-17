0b358778754e94deba6c82664f695d074b715de9
Jonas <jonas.maurer@campus.lmu.de>
2024-01-17 15:57:40 +0100

    camp extraction now more universal

--- backend/input_data_processing/add_camp_locations.py
+++ backend/input_data_processing/add_camp_locations.py
@@ -27,15 +27,27 @@ def add_camp_locations(round_data, folder_name, rows_shown, start_date, end_date
     """
     # sort dict by round number in descending order
     round_data.sort(key=lambda x: x["round"], reverse=True)
+    dtm_merged_df, modified_round_data = extract_camp_locations(round_data)
 
-    # filter by end_date: Just include the rounds which are covered by the end_date and start_date
+    # filter by dates: Just include the rounds which are covered by the end_date and start_date
     reformatted_start_date = date_format(start_date)
     reformatted_end_date = date_format(end_date)
-    round_data = [round for round in round_data if round["covered_from"] >= reformatted_start_date and round["covered_to"] <= reformatted_end_date]
-    
-    dtm_merged_df, modified_round_data, latest_survey_date = extract_camp_locations(round_data, rows_shown)
 
-    # sort round_data by round number in descending order to get latest info
+     # get rounds which are and which are not in the time period
+    rounds_not_in_time_period = [round_entry["round"] for round_entry in modified_round_data if round_entry["covered_from"] < reformatted_start_date or round_entry["covered_to"] > reformatted_end_date]
+    rounds_in_time_period = [round_entry["round"] for round_entry in modified_round_data if round_entry["covered_from"] >= reformatted_start_date and round_entry["covered_to"] <= reformatted_end_date]
+    
+    # modify modified_round_data to just inclide rounds in time period
+    modified_round_data = [round_entry for round_entry in modified_round_data if round_entry["covered_from"] >= reformatted_start_date and round_entry["covered_to"] <= reformatted_end_date] 
+
+
+    # delete the population columns and survey columns which do not have a round in the time period -> just keep rounds in time period
+    for round_number in rounds_not_in_time_period:
+        dtm_merged_df = dtm_merged_df.drop(columns=[f"population_round_{round_number}", f"survey_date_round_{round_number}"])
+
+
+    # latest round -> sort by IDP conflict number -> keep top 10 rows -> get latest survey date
+    # sort modified_round_data by round number in descending order to get latest info
     modified_round_data.sort(key=lambda x: x["round"], reverse=True)
     last_update = modified_round_data[0]["covered_to"]
     last_update_url = modified_round_data[0]["source"]
@@ -43,7 +55,20 @@ def add_camp_locations(round_data, folder_name, rows_shown, start_date, end_date
     # date of retrieval, which is the date when the script is executed. Format: YYYY-MM-DD HH:MM:SS
     retrieval_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
-    # get the key for population for the latest round
+    # sort by number_IDPs_conflict of latest round
+    dtm_merged_df.sort_values(by=f"population_round_{latest_round}", ascending=False, inplace=True)
+    dtm_merged_df = dtm_merged_df.reset_index(drop=True)
+    
+    # just keep top rows_shown entries
+    dtm_merged_df = dtm_merged_df.head(rows_shown)
+
+    # get latest survey date
+    latest_survey_date = dtm_merged_df[f"survey_date_round_{latest_round}"].tolist()
+    latest_survey_date.sort(reverse=True)
+    latest_survey_date = latest_survey_date[0]
+
+
+    # get the key for population for the latest round 
     latest_round_population_key = f"population_round_{latest_round}"
     latest_dtm_df = dtm_merged_df[["name", "region", "country", "latitude", "longitude", "location_type", "conflict_date", latest_round_population_key]]
     latest_dtm_df = latest_dtm_df.rename(columns={"name":"#name", latest_round_population_key: "population"})
@@ -53,20 +78,32 @@ def add_camp_locations(round_data, folder_name, rows_shown, start_date, end_date
 
     print("Successfully added camp locations to locations.csv")
 
+    # remove survey_dates for all rounds from dtm_merged_df except for the latest round
+    for round_number in rounds_in_time_period:
+        if round_number != latest_round:
+            dtm_merged_df = dtm_merged_df.drop(columns=[f"survey_date_round_{round_number}"])
+
+    print("modified round data")
+    print(modified_round_data)
+    print("latest_dtm_df")
+    print(latest_dtm_df)
+    print("dtm_merged_df: ")
+    print(dtm_merged_df)
+    print("dtm_mgerged_df columns: ")
+    print(dtm_merged_df.columns)
+
     return dtm_merged_df, modified_round_data, last_update_url, retrieval_date, last_update, reformatted_start_date, reformatted_end_date, latest_survey_date
 
 
 
-def extract_camp_locations(round_data, rows_shown):
+def extract_camp_locations(round_data):
     """
     The function extracts the camp locations with information from the DTM excel files and returns a dataframe with the camps and IDP numbers. Additionally it adds the total number of IDPs per period to round_data.
     Parameters:
         round_data (list): List of dictionaries with information about the rounds
-        rows_shown (int): Number of camps to be shown in the CSV file
     Returns:
         dtm_merged_df (dataframe): Merged dataframe with the camp locations
         round_data (list): List of dictionaries with information about the rounds. The total number of IDPs caused by conflict is added to the dictionary.
-        latest_survey_date (str): Latest survey date. Format: YYYY-MM-DD
     """
 
     # REMARK: neither the data format of the excel files nor the available columns is consistent, therefore the extraction of the data is not universally applicable.
@@ -81,18 +118,15 @@ def extract_camp_locations(round_data, rows_shown):
     current_dir = os.getcwd()
     # paths to files with camp information
     # docker path
-    paths_dtm = os.path.join(current_dir, "input_data_processing", "conflict_validation", "data_source")
-    # paths_dtm = os.path.join(current_dir, "conflict_validation", "data_source")
+    # paths_dtm = os.path.join(current_dir, "input_data_processing", "conflict_validation", "data_source")
+    paths_dtm = os.path.join(current_dir, "conflict_validation", "data_source")
 
-    # get round numbers from dict
-    round_numbers = []
-    for round in round_data:
-        round_numbers.append(round["round"])
 
     # create empty dataframe
     dtm_merged_df = pd.DataFrame()
 
-    for round_number in round_numbers:
+    for current_round in round_data: # rounds already in descending order
+        round_number = current_round["round"]
         excel_file = os.path.join(paths_dtm, f"DTM Ethiopia - Site Assessment Round {round_number}.xlsx")
 
         # extract information from excel files
@@ -148,80 +182,47 @@ def extract_camp_locations(round_data, rows_shown):
             print("Not possible to extract information for this round_number due to incompatibility of data formats!")
         
         # total number of IDPs caused by conflict
-        if round_number == 34:
-            total_IDP_conflict_number = dtm_df.loc[dtm_df["reason_for_displacement"] == "Conflict", "total_number_IDPs"].sum()
-        elif (round_number == 32 or round_number == 33):
-            # sum of column number_IDPs_conflict (formerly: Reason for Displacement (Individuals): Conflict)
-            total_IDP_conflict_number = dtm_df["number_IDPs_conflict"].sum()
-        else: 
-            print("Not possible to extract information for this round_number due to incompatibility of data formats!")
-        
-        
-        # add total_IDP_conflict_number to round_data
-        for round_data_dict in round_data:
-            if round_data_dict["round"] == round_number:
-                round_data_dict["total_IDP_conflict_number"] = total_IDP_conflict_number
+        if round_number == 34: # round 34 has no extra split up number of IDPs by reason for displacement. Do it by myself
+            # create new column "number_IDPs_conflict" and fill with values for "total_number_IDPs" if "reason_for_displacement" == "Conflict", otherwise with 0
+            dtm_df["number_IDPs_conflict"] = np.where(dtm_df["reason_for_displacement"] == "Conflict", dtm_df["total_number_IDPs"], 0)
+
+        # sum the number_IDPs_conflict column
+        total_IDP_conflict_number = dtm_df["number_IDPs_conflict"].sum()
+
+        # add total_IDP_conflict_number to correct round_data
+        for round_data_entry in round_data:
+            if round_data_entry["round"] == round_number:
+                round_data_entry["total_IDP_conflict_number"] = total_IDP_conflict_number
 
         
         # check if site_id unique. If not, then group by site_id and sum relevant columns
         if not dtm_df["site_id"].is_unique:
-            if round_number == 34:
-                # Group by "site_id" and sum "total_number_IDPs", keeping other columns unchanged (use first value with "first")
-                dtm_df = dtm_df.groupby("site_id").agg({
-                    "survey_date": "first",
-                    "reported_date": "first",
-                    "country": "first",
-                    "survey_round": "first",
-                    "site_name": "first",
-                    "region": "first",
-                    "reason_for_displacement": "first",
-                    "site_classification": "first",
-                    "settlement_type": "first",
-                    "total_number_IDPs": "sum"
-                }).reset_index()
-            elif (round_number == 32 or round_number == 33):
-                # Group by "site_id" and sum "total_number_IDPs" and "number_IDPs_conflict", keeping other columns unchanged
-                dtm_df = dtm_df.groupby("site_id").agg({
-                    "survey_date": "first",
-                    "reported_date": "first",
-                    "country": "first",
-                    "survey_round": "first",
-                    "site_name": "first",
-                    "longitude": "first",
-                    "latitude": "first",
-                    "region": "first",
-                    "main_reason_for_displacement": "first",
-                    "site_classification": "first",
-                    "settlement_type": "first",
-                    "total_number_IDPs": "sum",
-                    "number_IDPs_conflict": "sum"
-                }).reset_index()
-            else:
-                print("Not possible to extract information for this round_number due to incompatibility of data formats!")
+            # Group by "site_id" and sum "total_number_IDPs" and "number_IDPs_conflict", keeping other columns unchanged
+            dtm_df = dtm_df.groupby("site_id").agg({
+                "survey_date": "first",
+                "reported_date": "first",
+                "country": "first",
+                "survey_round": "first",
+                "site_name": "first",
+                "longitude": "first",
+                "latitude": "first",
+                "region": "first",
+                "main_reason_for_displacement": "first",
+                "site_classification": "first",
+                "settlement_type": "first",
+                "total_number_IDPs": "sum",
+                "number_IDPs_conflict": "sum"
+            }).reset_index()
 
 
-        # first iteration
-        if dtm_merged_df.empty:
+        if dtm_merged_df.empty:  # first iteration through the loop
             # Keep only the rows with settlement_type: Spontaneous camp/site or Planned camp/site
+            # these camps of the latest data (round 34) are the baseline 
             dtm_df = dtm_df.loc[(dtm_df["settlement_type"] == "Spontaneous camp/site") | (dtm_df["settlement_type"] == "Planned camp/site")]
-            
-            # sort by number_IDPs_conflict
-            dtm_df.sort_values(by="total_number_IDPs", ascending=False, inplace=True)
-            dtm_df = dtm_df.reset_index(drop=True)
-
-            # just keep top rows_shown entries
-            dtm_df = dtm_df.head(rows_shown)
-
-            # latest reported_date  
-            survey_dates = dtm_df["survey_date"].tolist()
-            survey_dates.sort(reverse=True)
-            latest_survey_date = survey_dates[0]
-
-
-        # check if dtm_merged_df is empty
         else:
             # get site ids as list
             relevant_site_ids = dtm_merged_df["site_id"].tolist()
+            # just keep the rows with site_id of our baseline (round 34)
             dtm_df = dtm_df[dtm_df["site_id"].isin(relevant_site_ids)]
         
 
@@ -231,35 +232,37 @@ def extract_camp_locations(round_data, rows_shown):
 
         if "latitude" in dtm_df.columns:
             # number_IDPs_conflict for each camp (additionally there could be other reasons why IDPs flee. We just focus on conflicts)
-            dtm_df = dtm_df[['site_id', 'site_name', 'region', 'country', 'latitude', 'longitude', 'location_type', 'conflict_date', 'number_IDPs_conflict']]
+            dtm_df = dtm_df[['site_id', 'site_name', 'region', 'country', 'latitude', 'longitude', 'location_type', 'survey_date', 'conflict_date', 'number_IDPs_conflict']]
             # Rename the columns
             dtm_df = dtm_df.rename(columns={"site_name": "name", "number_IDPs_conflict": "population"})
         else: 
-            dtm_df = dtm_df[['site_id', 'site_name', 'region', 'country', 'location_type', 'conflict_date', 'total_number_IDPs']]
+            dtm_df = dtm_df[['site_id', 'site_name', 'region', 'country', 'location_type', 'survey_date', 'conflict_date', 'number_IDPs_conflict']]
             # Rename the columns
-            dtm_df = dtm_df.rename(columns={"site_name": "name","total_number_IDPs": "population"})
+            dtm_df = dtm_df.rename(columns={"site_name": "name", "number_IDPs_conflict": "population"})
 
         dtm_df_premerged = pd.DataFrame()
 
-        if dtm_merged_df.empty:
+        if dtm_merged_df.empty: # first iteration through the loop
             dtm_merged_df = dtm_df
-            dtm_merged_df = dtm_merged_df.rename(columns={"population": f"population_round_{round_number}"})
+            dtm_merged_df = dtm_merged_df.rename(columns={"population": f"population_round_{round_number}", "survey_date": f"survey_date_round_{round_number}"})
         else:
-            dtm_df_premerged = dtm_df[['site_id', 'latitude', 'longitude', 'population']]
-            dtm_df_premerged = dtm_df_premerged.rename(columns={"population": f"population_round_{round_number}"})
+            dtm_df_premerged = dtm_df[['site_id', 'latitude', 'longitude', 'population', 'survey_date']]
+            dtm_df_premerged = dtm_df_premerged.rename(columns={"population": f"population_round_{round_number}", "survey_date": f"survey_date_round_{round_number}"})
         
 
-        # check if column latitude and longitude is in dtm_merged_df. Then drop
+        # check if column latitude and longitude is in dtm_merged_df, then drop
         if "latitude" in dtm_merged_df.columns:
             # drop column in dtm_df_premerged
             dtm_df_premerged = dtm_df_premerged.drop(columns=["latitude", "longitude"])
         
+        # merge (append) dataframes if not empty
         if not dtm_df_premerged.empty:
-            dtm_merged_df = pd.merge(dtm_merged_df, dtm_df_premerged, on="site_id", how="left")
-        
-        dtm_merged_df = dtm_merged_df.rename(columns={"population": f"population_round_{round_number}"})
+            dtm_merged_df = pd.merge(dtm_merged_df, dtm_df_premerged, on="site_id", how="inner") # inner join on site_id
+        # rename current round population and survey_date column
+        # TODO: necessary??
+        dtm_merged_df = dtm_merged_df.rename(columns={"population": f"population_round_{round_number}", "survey_date": f"survey_date_round_{round_number}"})
 
         print(f"round_number: {round_number}, total_IDP_conflict_number: {total_IDP_conflict_number}")
-    # print(dtm_merged_df)
+    print(dtm_merged_df.sort_values(by="site_id"))
 
-    return dtm_merged_df, round_data, latest_survey_date
+    return dtm_merged_df, round_data
